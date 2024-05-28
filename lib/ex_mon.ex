d742b6436d2f15adc4aceedf7ad47249c985f3f3
OtavioMiranda23 <otaviomiranda@usp.br>
2024-05-27 23:28:45 -0300

    feat: add movimentos iniciais

--- lib/ex_mon.ex
+++ lib/ex_mon.ex
@@ -1,5 +1,21 @@
 defmodule ExMon do
+  alias ExMon.{Game, Player}
+  alias ExMon.Game.{Actions, Status}
+  @computer_game "Robotiniki"
+
   def create_player(name, move_rnd, move_avg, move_heal) do
-    ExMon.Player.build(name, move_rnd, move_avg, move_heal)
+    Player.build(name, move_rnd, move_avg, move_heal)
+  end
+
+  def start_game(player) do
+    @computer_game
+    |> create_player(:punch, :kick, :heal)
+    |> Game.start(player)
+
+    Status.print_round_message()
+  end
+
+  def make_move(move) do
+    Actions.fetch_move(move)
   end
 end
